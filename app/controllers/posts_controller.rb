a1b61f069d362fc1cdc8d03cfd5b7a37b0b7a8b3
NaKaNoNaoKi-3014 <v21e3014@oita-u.ac.jp>
2022-06-03 04:23:00 +0000

    Lesson07の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,9 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'desc') # postsテーブルのすべてのレコードを持ってきて新しい順にソート
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
 
 end

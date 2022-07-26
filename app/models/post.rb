852b151031f424d3d8ddf6f184a8bccd868c0948
NaKaNoNaoKi-3014 <v21e3014@oita-u.ac.jp>
2022-07-26 05:56:00 +0000

    Lesson15の課題

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,11 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
+    belongs_to :user
     validates :title, presence: true, length: {minimum: 3}
     validates :body, presence: true
 end
